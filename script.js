7d54fb6d40fa460801bf286a8fd6dc19848c7959
Victor Mendonca <victor.men.dev@gmail.com>
2021-06-04 18:26:58 -0300

    Req 7

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let listaTarefas = document.querySelector('#lista-tarefas')
+let listaTarefas = document.querySelector('#lista-tarefas');
 
 function addTarefa(){
     let tarefa = document.querySelector('#texto-tarefa').value;
     let task = document.createElement('li');
     task.innerText = tarefa;
     listaTarefas.appendChild(task);
+    task.addEventListener('click', pintaCinza);
+    function pintaCinza(aleatory){
+        task.style.backgroundColor = 'rgb(128, 128, 128)'
+    }
     document.getElementById('texto-tarefa').value='';
 }
